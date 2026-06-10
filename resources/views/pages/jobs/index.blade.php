e402a11d48684a4f48e0dc940985d52617bb0ffb
root <root@localhost.localdomain>
2026-06-10 12:24:49 +0800

    feat(seo): add city job landing pages

--- resources/views/pages/jobs/index.blade.php
+++ resources/views/pages/jobs/index.blade.php
@@ -96,7 +96,7 @@
 
             {{-- Search Bar --}}
             <div class="bg-white rounded-2xl shadow-lg border border-slate-200 p-3 sm:p-4">
-                <form action="{{ route('jobs.index') }}" method="GET" class="flex flex-col md:flex-row gap-3">
+                <form action="{{ isset($landing) ? route('jobs.landing', request()->route('slug')) : route('jobs.index') }}" method="GET" class="flex flex-col md:flex-row gap-3">
                     <div class="flex-1 relative">
                         <svg class="absolute left-4 top-1/2 -translate-y-1/2 w-5 h-5 text-slate-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"/>
@@ -217,7 +217,7 @@
                         </a>
                     </div>
 
-                    <form action="{{ route('jobs.index') }}" method="GET" class="space-y-6" x-data="jobFilters()" @change.debounce.250ms="apply($el)" @submit.prevent="apply($el)">
+                    <form action="{{ isset($landing) ? route('jobs.landing', request()->route('slug')) : route('jobs.index') }}" method="GET" class="space-y-6" x-data="jobFilters()" @change.debounce.250ms="apply($el)" @submit.prevent="apply($el)">
                         @if($filters->keyword ?? false)
                             <input type="hidden" name="keyword" value="{{ $filters->keyword }}">
                         @endif
